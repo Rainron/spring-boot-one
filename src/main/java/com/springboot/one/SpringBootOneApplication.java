222bfdf86345922ae90652212ea0e3baf10af776
Rainron <519961939@qq.com>
2019-05-24 22:47:36 +0800

    first commit spring-boot-one

---
+++ src/main/java/com/springboot/one/SpringBootOneApplication.java
@@ -0,0 +1,26 @@
+package com.springboot.one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("hello")
+public class SpringBootOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOneApplication.class, args);
+    }
+
+
+    @RequestMapping(value = "/{id}/{name}",method = RequestMethod.GET)
+    public String hello(@PathVariable("id")String id,@PathVariable("name")String name){
+
+        return String.format("{\"message\":\"hello %s %s\"}",id,name);
+
+    }
+}
